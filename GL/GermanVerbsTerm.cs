ccf12fbfa1324809fd0be0f09bc5d51c3f1a46ac
Solann PUYGRENIER <spuygrenier@cfai-formation.fr>
2022-07-10 11:21:08 +0200

    feat(Regular verbs declination): add rule of du

--- GL/GermanVerbsTerm.cs
+++ GL/GermanVerbsTerm.cs
@@ -1,5 +1,9 @@
 ﻿namespace GL
 {
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <link src="https://www.thegermanprofessor.com/present-tense-verbs/">Source 2</link>
     public static class GermanVerbsTerm
     {
 
@@ -17,29 +21,38 @@
         };
         public static Dictionary<int, List<string>> InPresent(string verb)
         {
-            string ending = EndingDigitOfVerbs(2,verb);
-            if (ending.Equals("en")) return RegularDeclinaisonOfVerbTerminatedBy_EN(verb);
+            string ending = EndingDigitOfVerbs(2, verb);
+            Dictionary<int, List<string>> verbsResult = new();
+            bool IsSlightVariationFor_DuPronom = IsVariationEndingFor_DuPronon(verb);
+            if (ending.Equals("en")) return RegularDeclinaisonOfVerbTerminatedBy_EN(verb, IsSlightVariationFor_DuPronom);
             else
             {
                 ending = EndingDigitOfVerbs(3, verb);
                 if (ending.Equals("eln"))
                 {
-                    return RegularDeclinaisonOfVerbTerminatedBy_ELN(verb);
+                    verbsResult = RegularDeclinaisonOfVerbTerminatedBy_ELN(verb, IsSlightVariationFor_DuPronom);
                 }
                 else if (ending.Equals("ern"))
                 {
-                    return RegularDeclinaisionOfVerbsTerminatedVy_ERN(verb);
+                    verbsResult = RegularDeclinaisionOfVerbsTerminatedVy_ERN(verb, IsSlightVariationFor_DuPronom);
                 }
             }
-            return null;
+            return verbsResult;
         }
 
-        private static Dictionary<int, List<string>> RegularDeclinaisionOfVerbsTerminatedVy_ERN(string verb)
+        private static bool IsVariationEndingFor_DuPronon(string verb)
         {
+            List<char> particluarEndingOfDu = new List<char> { 'z', 's', 'x','ß' };
+            return particluarEndingOfDu.Contains(verb.ElementAt(verb.Length -2)) || particluarEndingOfDu.Contains(verb.ElementAt(verb.Length - 3));
+        }
+
+        private static Dictionary<int, List<string>> RegularDeclinaisionOfVerbsTerminatedVy_ERN(string verb, bool isVariationFor_DuPronom)
+        {
+            string duEnding = GetDuEnding(isVariationFor_DuPronom);
             string prefix = verb.Substring(0, verb.Length - 1);
             return new Dictionary<int, List<string>> {
                         {1, new List<string>{ "ich", prefix + "e" }},
-                        {2, new List<string>{ "du", prefix + "st" }},
+                        {2, new List<string>{ "du", prefix + duEnding}},
                         {3, new List<string>{ "er", prefix + "t" }},
                         {4, new List<string>{ "sie", prefix + "t" }},
                         {5, new List<string>{ "es", prefix + "t" }},
@@ -50,13 +63,15 @@
                     };
         }
 
-        private static Dictionary<int, List<string>> RegularDeclinaisonOfVerbTerminatedBy_ELN(string verb)
+        private static Dictionary<int, List<string>> RegularDeclinaisonOfVerbTerminatedBy_ELN(string verb, bool isVariationFor_DuPronom)
         {
+            string duEnding = GetDuEnding(isVariationFor_DuPronom);
+
             string prefix = verb.Substring(0, verb.Length - 1);
             return new Dictionary<int, List<string>>
                     {
                         { 1, new List<string> { "ich", prefix + "e" } },
-                        { 2, new List<string> { "du", prefix+ "st" } },
+                        { 2, new List<string> { "du", prefix+ duEnding } },
                         { 3, new List<string> { "er", prefix + "t" } },
                         { 4, new List<string> { "sie", prefix + "t" } },
                         { 5, new List<string> { "es", prefix + "t" } },
@@ -72,12 +87,14 @@
             return verb.Substring(verb.Length - digitFromEnd, digitFromEnd);
         }
 
-        private static Dictionary<int, List<string>> RegularDeclinaisonOfVerbTerminatedBy_EN(string verb)
+        private static Dictionary<int, List<string>> RegularDeclinaisonOfVerbTerminatedBy_EN(string verb, bool isVariationFor_DuPronom)
         {
+            string duEnding = GetDuEnding(isVariationFor_DuPronom);
+
             string prefix = verb.Substring(0, verb.Length - 2);
             return new Dictionary<int, List<string>>() {
                     { 1, new List<string> { "ich", prefix+ "e" }},
-                    { 2, new List<string> { "du", prefix+ "st" }},
+                    { 2, new List<string> { "du", prefix+ duEnding }},
                     { 3, new List<string> { "er", prefix+ "t" }},
                     { 4, new List<string> { "sie", prefix+ "t" }},
                     { 5, new List<string> { "es", prefix+ "t" }},
@@ -86,6 +103,14 @@
                     { 8, new List<string> { "sie", prefix+ "en" }},
                     { 9, new List<string> { "Sie", prefix+ "en" }}
                 };
+        }
+
+        private static string GetDuEnding(bool isVariationFor_DuPronom)
+        {
+            string duEnding = "st";
+            if (isVariationFor_DuPronom) duEnding = "t";
+            else duEnding = "st";
+            return duEnding;
         }
     }
 }
--- TestProject/UnitTest1.cs
+++ TestProject/UnitTest1.cs
@@ -8,6 +8,7 @@ namespace TestProject
         [Fact]
         public void Test1()
         {
+            // Spelen = to play = jouer
             Dictionary<int,List<string>> regularVerbsEndingByEn = GermanVerbsTerm.InPresent("spielen");
             Dictionary<int, List<string>> correctRegularVerbsEndingBy_EN = new Dictionary<int, List<string>> {
                 {1, new List<string>{ "ich", "spiele" }},
@@ -20,7 +21,7 @@ namespace TestProject
                 {8, new List<string>{ "sie", "spielen" }},
                 {9, new List<string>{ "Sie", "spielen" }},
             };
-
+            // klingeln = to ring = sonner
             Dictionary<int, List<string>> regularVerbsEndingBy_ELN = GermanVerbsTerm.InPresent("klingeln");
             Dictionary<int, List<string>> correctRegularVerbsEndingBy_ELN = new Dictionary<int, List<string>> {
                 {1, new List<string>{ "ich", "klingele" }},
@@ -33,7 +34,7 @@ namespace TestProject
                 {8, new List<string>{ "sie", "klingeln" }},
                 {9, new List<string>{ "Sie", "klingeln" }},
             };
-
+            // Kletern = to climb = escalader
             Dictionary<int, List<string>> regularVerbsEndingBy_ERN = GermanVerbsTerm.InPresent("klettern");
             Dictionary<int, List<string>> correctRegularVerbsEndingBy_ERN = new Dictionary<int, List<string>> {
                 {1, new List<string>{ "ich", "klettere" }},
@@ -46,16 +47,59 @@ namespace TestProject
                 {8, new List<string>{ "sie", "klettern" }},
                 {9, new List<string>{ "Sie", "klettern" }},
             };
+            // Sitzen = to sit = s'assoir
+            Dictionary<int, List<string>> regularRegularVerbsHaving_z = GermanVerbsTerm.InPresent("sitzen");
+            Dictionary<int, List<string>> correctRegularVerbsHaving_z = new Dictionary<int, List<string>> {
+                {1, new List<string>{ "ich", "sitze" }},
+                {2, new List<string>{ "du", "sitzt" }},
+                {3, new List<string>{ "er", "sitzt" }},
+                {4, new List<string>{ "sie", "sitzt" }},
+                {5, new List<string>{ "es", "sitzt" }},
+                {6, new List<string>{ "wir", "sitzen" }},
+                {7, new List<string>{ "ihr", "sitzt" }},
+                {8, new List<string>{ "sie", "sitzen" }},
+                {9, new List<string>{ "Sie", "sitzen" }},
+            };
+            //Faxen = to fax = faxer
+            Dictionary<int, List<string>> regularRegularVerbsHaving_x = GermanVerbsTerm.InPresent("faxen");
+            Dictionary<int, List<string>> correctRegularVerbsHaving_x = new Dictionary<int, List<string>> {
+                {1, new List<string>{ "ich", "faxe" }},
+                {2, new List<string>{ "du", "faxt" }},
+                {3, new List<string>{ "er", "faxt" }},
+                {4, new List<string>{ "sie", "faxt" }},
+                {5, new List<string>{ "es", "faxt" }},
+                {6, new List<string>{ "wir", "faxen" }},
+                {7, new List<string>{ "ihr", "faxt" }},
+                {8, new List<string>{ "sie", "faxen" }},
+                {9, new List<string>{ "Sie", "faxen" }},
+            };
 
+            // ALT + 225 for ﬂ
+            // Gieﬂt or Giest = to pour / spill = verser
+            Dictionary<int, List<string>> regularRegularVerbsHaving_ﬂ = GermanVerbsTerm.InPresent("gieﬂen");
+            Dictionary<int, List<string>> correctRegularVerbsHaving_ﬂ = new Dictionary<int, List<string>> {
+                {1, new List<string>{ "ich", "gieﬂe" }},
+                {2, new List<string>{ "du", "gieﬂt" }},
+                {3, new List<string>{ "er", "gieﬂt" }},
+                {4, new List<string>{ "sie", "gieﬂt" }},
+                {5, new List<string>{ "es", "gieﬂt" }},
+                {6, new List<string>{ "wir", "gieﬂen" }},
+                {7, new List<string>{ "ihr", "gieﬂt" }},
+                {8, new List<string>{ "sie", "gieﬂen" }},
+                {9, new List<string>{ "Sie", "gieﬂen" }},
+            };
 
             foreach ( var key in regularVerbsEndingByEn.Keys) Assert.Equal(correctRegularVerbsEndingBy_EN[key], regularVerbsEndingByEn[key] );
 
             foreach (var key in regularVerbsEndingBy_ELN.Keys) Assert.Equal(correctRegularVerbsEndingBy_ELN[key], regularVerbsEndingBy_ELN[key] );
             
-            foreach (var key in regularVerbsEndingBy_ELN.Keys) Assert.Equal(correctRegularVerbsEndingBy_ERN[key], regularVerbsEndingBy_ERN[key]);
-            
+            foreach (var key in regularVerbsEndingBy_ERN.Keys) Assert.Equal(correctRegularVerbsEndingBy_ERN[key], regularVerbsEndingBy_ERN[key]);
 
+            foreach (var key in regularRegularVerbsHaving_z.Keys) Assert.Equal(correctRegularVerbsHaving_z[key], regularRegularVerbsHaving_z[key]);
 
+            foreach (var key in regularRegularVerbsHaving_x.Keys) Assert.Equal(correctRegularVerbsHaving_x[key], regularRegularVerbsHaving_x[key]);
+
+            foreach (var key in regularRegularVerbsHaving_ﬂ.Keys) Assert.Equal(correctRegularVerbsHaving_ﬂ[key], regularRegularVerbsHaving_ﬂ[key]);
         }
     }
 }
